dc451bfc98978d825843d920256673967cb12b7e
Sam Slotsky <saxosamo@gmail.com>
2016-07-13 22:57:25 -0500

    NEW SINCE LAST VIDEO

--- app/reducer.js
+++ app/reducer.js
@@ -6,18 +6,63 @@ const initialState = Map({
   subjects: List()
 })
 
+function mapSubjects(state, action, mutate) {
+  return state.get('subjects').map(s => {
+    if (s.get('name') !== action.name) {
+      return s
+    }
+
+    return mutate(s)
+  })
+}
+
+function readAllTweets(state, action) {
+  const subjects = mapSubjects(state, action, s =>
+    s.merge({
+      readTweets: s.get('unreadTweets').concat(s.get('readTweets')),
+      unreadTweets: List()
+    })
+  )
+
+  return state.merge({ subjects })
+}
+
+function viewMoreTweets(state, action) {
+  const subjects = mapSubjects(state, action, s =>
+    s.merge({
+      visibleCount: s.get('visibleCount') + 50
+    })
+  )
+
+  return state.merge({ subjects })
+}
+
+function untrackSubject(state, action) {
+  const subjects = state.get('subjects').filter(s => s.get('name') !== action.name)
+  return state.merge({ subjects })
+}
+
 function trackNewSubject(state, action) {
   const subjects = state.get('subjects')
   if (subjects.some(s => s.get('name') === action.name)) {
     return state
   }
 
-  const subject = Map({ name: action.name, tweets: List(), visibleCount: 50 })
+  const subject = Map({
+    name: action.name,
+    readTweets: List(),
+    unreadTweets: List(),
+    visibleCount: 50
+  })
+
   return state.merge({
     subjects: subjects.push(subject)
   })
 }
 
 export default resolve(initialState, {
-  [actionTypes.TRACK_SUBJECT]: trackNewSubject
+  [actionTypes.TRACK_SUBJECT]: trackNewSubject,
+  [actionTypes.READ_ALL_TWEETS]: readAllTweets,
+  [actionTypes.VIEW_MORE_TWEETS]: viewMoreTweets,
+  [actionTypes.UNTRACK_SUBJECT]: untrackSubject
 })
