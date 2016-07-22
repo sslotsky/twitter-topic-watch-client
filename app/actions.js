9ab1bcd52af423b32d31cefddd0fb0cdb783f8f7
Sam Slotsky <saxosamo@gmail.com>
2016-07-21 21:30:15 -0500

    video 2 finished

--- app/actions.js
+++ app/actions.js
@@ -1,9 +1,25 @@
 import * as actionTypes from './constants'
+import socket, { channel } from './socket'
 
 export function track(name) {
   return {
     type: actionTypes.TRACK_SUBJECT,
     name
+  }
+}
+
+export function listen(name) {
+  return dispatch => {
+    dispatch(track(name))
+
+    socket.emit('track', name)
+    channel(name).on('tweets', tweets => {
+      dispatch({
+        type: actionTypes.TWEETS_RECEIVED,
+        tweets,
+        name
+      })
+    })
   }
 }
 
--- app/components/Tracker.jsx
+++ app/components/Tracker.jsx
@@ -1,16 +1,46 @@
 import React, { PropTypes, Component } from 'react'
+import Tweets from './Tweets'
+import { readAllTweets } from '../actions'
+import { connect } from 'react-redux'
 
-export default class Tracker extends Component {
+export class Tracker extends Component {
   static propTypes = {
-    subject: PropTypes.object.isRequired
+    subject: PropTypes.object.isRequired,
+    readAll: PropTypes.func.isRequired
+  }
+
+  readAll() {
+    const { subject, readAll } = this.props
+    readAll(subject.name)
+    this.scrollTop.scrollTop = 0
+  }
+
+  unreadTweets() {
+    const { unreadTweets } = this.props.subject
+    return (
+      <a className='pure-button pure-button-primary' onClick={() => this.readAll()}>
+        {`${unreadTweets.length} unread tweets (read more)`}
+      </a>
+    )
   }
 
   render() {
-    const { name } = this.props.subject
+    const { name, readTweets } = this.props.subject
     return (
       <div className='tracker'>
-        <h2>{name}</h2>
+        <div className='subject-header'>
+          <h2>{name}</h2>
+          {this.unreadTweets()}
+        </div>
+        <div className='tweets' ref={(node) => { this.scrollTop = node }}>
+          <Tweets tweets={readTweets} />
+        </div>
       </div>
     )
   }
 }
+
+export default connect(
+  () => ({}),
+  { readAll: readAllTweets }
+)(Tracker)
