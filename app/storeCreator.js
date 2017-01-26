9a62cafd15e44c1555c7974b0958ed410f75c0db
Samuel Slotsky <sam.slotsky@reachlocal.com>
2017-01-25 23:12:16 -0600

    client & server app

--- app/storeCreator.js
+++
@@ -1,5 +0,0 @@
-export default function createStore(initialState, registrar) {
-  return (state = initialState, action, handlers = registrar(state, action)) =>
-    handlers[action.type] ?
-      handlers[action.type]() : state;
-}
